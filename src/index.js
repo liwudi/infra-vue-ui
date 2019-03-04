526f1ddfda55c6f36f302be53062f95d1abe6413
liwudi <liwudi@idvert.com>
2019-03-04 11:02:20 +0800

    添加Vue的原型方法$alertMessage

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import SinglePicker from '../packages/single-picker/index'
 import Button from '../packages/Button/index'
 import Input from '../packages/Input/index'
 import Select from '../packages/Select/index'
+
+// 消息提示
 import AlertMessage from '../packages/Alert/index'
 
 const components = [
@@ -18,9 +20,10 @@ const components = [
 
 const install = function (Vue, opts = {}) {
   components.map(component => {
-    console.log(component.name);
+    console.log(component.name)
     Vue.component(component.name, component)
   })
+  Vue.prototype.$alertMessage = AlertMessage
 }
 
 const infraVueUI = {
