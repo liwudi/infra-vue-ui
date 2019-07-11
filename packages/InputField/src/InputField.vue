1a442e099389c790f8dca6b5e26e0a7af0403399
Wangyinping <553475846@qq.com>
2019-07-11 16:02:03 +0800

    销售App InputField

---
+++ packages/InputField/src/InputField.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="koala-input-box">
+    <label class="koala-label">{{label}}</label>
+    <input :style="{width: inputWidth}" class="koala-input v-validate" v-bind="$props" @input="handleInput" @blur="blur" />
+  </div>
+</template>
+<script>
+  export default{
+    name: 'infra-input-field',
+    props: {
+      value: [String, Number],
+      placeholder: String,
+      name: String,
+      id: String,
+      maxlength: Number,
+      minlength: Number,
+      readonly: Boolean,
+      disabled: Boolean,
+      inputWidth: {
+        type: String
+      },
+      type: {
+        type: String,
+        default: 'text'
+      },
+      rows: {
+        type: Number,
+        default: 2
+      },
+      autoComplete: {
+        type: String,
+        default: 'off'
+      },
+      validateEvent: {
+        type: Boolean,
+        default: true
+      },
+      suffixIcon: String,
+      label: String
+    },
+    mounted () {
+
+    },
+    methods: {
+      handleInput (event) {
+        const value = event.target.value;
+        this.$emit('input', value);
+      },
+      blur () {
+        this.$emit('blur');
+        //      window.scroll(0,0);
+      }
+    }
+  }
+</script>
+<!-- Add 'scoped' attribute to limit CSS to this component only.-->
+<style scoped lang="css">
+  .koala-input-box{
+    overflow:hidden;
+  }
+  .koala-input-box .koala-input{
+    float: right;
+    height: 1.02rem;
+    padding-bottom: 0.12rem;
+    width: 70%;
+    text-align: right;
+    border:0;
+    outline:0;
+    padding-right:0.67rem;
+    font-size: 0.3rem;
+    color: #465582;
+  }
+  .koala-input-box input::-webkit-input-placeholder{
+    font-size: 0.3rem;
+    color:#CDD4E1;
+  }
+</style>
