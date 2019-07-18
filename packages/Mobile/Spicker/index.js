d87a81ee22da04817ef31adf30bbc70352d154c0
Wangyinping <553475846@qq.com>
2019-07-18 10:17:11 +0800

    spicker

---
+++ packages/Mobile/Spicker/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by kaola on 2019-07-11.
+ */
+import Spicker from './src/Spicker';
+
+Spicker.install = function (Vue) {
+  Vue.component(Spicker.name, Spicker);
+};
+
+export default Spicker;
